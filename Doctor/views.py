b64fd98f42e043a0096a284485ab77a13549186d
Anirudh A V <anirudh.av02@gmail.com>
2022-12-06 10:29:47 +0530

    Doctor views defined

--- Doctor/views.py
+++ Doctor/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request, "Doctor/index.html")
+
+def doctor(request):
+    return render(request, "Doctor/doctor.html")
+
+def department(request):
+    return render(request, "Doctor/department.html")
+
+def dutytime(request):
+    return render(request, "Doctor/dutytime.html")
+
+def doctordepartment(request):
+    return render(request, "Doctor/doctordepartment.html")
+
+# Compare this snippet from Doctor\forms.py:
