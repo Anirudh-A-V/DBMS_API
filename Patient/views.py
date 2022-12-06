2d942f51ab476a3a505c648778bc6bda32004ad1
Anirudh A V <anirudh.av02@gmail.com>
2022-12-06 10:30:13 +0530

    Patient views defined

--- Patient/views.py
+++ Patient/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request, "Patient/index.html")
+
+def patient(request):
+    return render(request, "Patient/patient.html")
+
+def appointment(request):
+    return render(request, "Patient/appointment.html")
+
+def payment(request):
+    return render(request, "Patient/payment.html")
+
+def test(request):
+    return render(request, "Patient/test.html")
+
+def testreport(request):
+    return render(request, "Patient/testreport.html")
+
+def companion(request):
+    return render(request, "Patient/companion.html")
